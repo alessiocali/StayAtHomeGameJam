542c0d4cabcdee0340b8e77011070ed52b91b728
AMascherin <amascherin@gmail.com>
2020-03-21 22:52:51 +0100

    Added collider to prefab. Added tag to scene. First InputManager draft

---
+++ Assets/InputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private int layer_mask;
+
+    private void Awake()
+    {
+        layer_mask = LayerMask.GetMask("Tile");
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Debug.DrawRay(ray.origin, ray.direction * 200f, Color.red,5);
+
+            if (Physics.Raycast(ray, out hit, 200f,layer_mask))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
+    }
+    
+
+}
