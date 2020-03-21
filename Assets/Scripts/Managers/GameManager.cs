bbc327bcb5fcc69610b879332fd1695cdc6fed5e
Alessio Cali <alexander92@hotmail.it>
2020-03-21 13:46:35 +0200

    Added core infrastructure, fixed missing meta files

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,6 +4,23 @@ using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    private Dictionary<GridTile.TileIndex, GridTile> Grid;
+
+    public List<GridTile> GetTilesAround(GridTile.TileIndex tileIndex)
+    {
+        return new List<GridTile>();
+    }
+
+    private List<Occupant> GetAllOccupants()
+    {
+        return new List<Occupant>();
+    }
+
+    private void UpdateAllOccupants()
+    {
+
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +30,6 @@ public class GameManager : Singleton<GameManager>
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateAllOccupants();
     }
 }
