38f3c07d46bec66a5ae3ec978f7cb12755dbe0ff
AMascherin <amascherin@gmail.com>
2020-03-21 19:33:47 +0100

    Progress on Grandma, some changes in variables of gridmap and gridtile

--- Assets/Scripts/Engine/Occupants/Grandma.cs
+++ Assets/Scripts/Engine/Occupants/Grandma.cs
@@ -1,17 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Grandma : Character 
+public class Grandma : Character
 {
-    private enum GrandmaChoices{
+    private enum GrandmaChoices
+    {
         Moving,
         Waiting,
         Sneezing
     };
 
+    private int nextAction = 0;
+    private readonly int enumLength = Enum.GetNames(typeof(GrandmaChoices)).Length;
+
+
     protected override UpdateTurnResult UpdateTurnInternal() {
-        List<GridTile> tilesList = GameManager.Instance.GridMap.GetTilesAround(CurrentTileIndex);
+
+        switch (nextAction) {
+            case (int)GrandmaChoices.Moving:
+                List<GridTile> tilesList = GameManager.Instance.GridMap.GetWalkableTilesAround(CurrentTileIndex);
+                if (tilesList.Count > 0)
+                {
+                    GridTile nextTile = tilesList[UnityEngine.Random.Range(0, tilesList.Count)];
+
+                    MoveToTile(nextTile.Index);
+
+                    //TODO: LookAt rotation given by Vector3 GameManager.instance.GetRotation (currentTile, desiredTile)
+                    transform.LookAt(Vector3.zero);
+                }
+
+                break;
+
+            case (int)GrandmaChoices.Waiting:
+                break;
+
+            case (int)GrandmaChoices.Sneezing:
+                //TODO: spawn
+                break;
+        
+        
+        }
+        nextAction = (nextAction + 1) % enumLength;
         return UpdateTurnResult.Completed;
     }
 }
