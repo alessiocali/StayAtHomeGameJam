ff6baa5b7eebcbbc2921acb622dd3a9683abf3e3
Rossella <s243249@studenti.polito.it>
2020-03-22 11:49:16 +0100

    Aggiunto menù di pausa

---
+++ Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause_Menu : MonoBehaviour {
+    
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject HUD;
+
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.Escape)) {
+            if(GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume() {
+        pauseMenuUI.SetActive(false);
+        GameIsPaused = false;
+        HUD.SetActive(true);
+    }
+
+    void Pause() {
+        pauseMenuUI.SetActive(true);
+        GameIsPaused = true;
+        HUD.SetActive(false);
+    }
+
+    public void quitGame() {
+        Application.Quit();
+    }
+}
