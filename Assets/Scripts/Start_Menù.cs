f3a1eba2235ddb80fd3c5008f9d5200fc7dc8c31
Rossella <s243249@studenti.polito.it>
2020-03-22 11:09:47 +0100

    Aggiunto MainMenù e freccetta

---
+++ Assets/Scripts/Start_Menù.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Start_Menù : MonoBehaviour
+{
+    public void PlayGame() {
+        //SceneManager.LoadScene("Game_Manager");
+        Debug.Log("START");
+    }
+
+    public void QuitGame() {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
